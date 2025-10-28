f216193c3a5e834e036a76789d93c56769f8cbef
Wellington P. Silva <welltech.devworks@gmail.com>
2025-10-28 07:43:17 -0300

    Implement LED control and temperature sensor reading
    Added LED indication and temperature sensor reading logic.

--- main.py
+++ main.py
@@ -1,32 +1,47 @@
-import serial as ser
+# leitura  do  led de indicação de energia
+led = Pin(4, Pin.OUT) # pino 4 e uma saída /usar um led pisca para facilitar a visualização
+
+print("seja bem vindo wellington e olá cobaia!")
+time.sleep(1)
+print("iniciando teste de solo...")
+
 import time
-import binascii as cii
-import LoRa
-import pandas as pd
-import socket
+import serial as ser
+
+from machine import Pin # controle dos  pinos
+from time import sleep # controle de delay do tempo
 
 # Ajuste a porta conforme o sistema
-esp = ser.Serial('COM5', 115200, timeout=1)
+esp = ser.Serial('COM5', 115200, timeout = 1)
 # Espera o ESP32 inicializar
-time.sleep(2)
+time.sleep(1)
 
-# se ok / Envia comando
-esp.write(b"iniciar/n")
+# configurações de pinos
 
-# Ler, e responder todo o sistema
-resposta = esp.readline().decode().strip()
-print("resposta do ESP32", resposta)
 
-#iniciar contagem regressiva
+#  leitura do sensor de temperatura
+tempe = Pin(6, Pin.IN) # pino 6 e a entrada da informação da temperatura
+#leitura do módulo do giroscópio
+giro = Pin(7,Pin.IN) # pino 7 e a entrada
 
-print("iniciar contagem")
+# leitor de temperatura
+while True:
+    valor_tempe = tempe.value() #determina que o sensor será lido como um livro
+    if 0 <= valor_tempe <= 30: # comparação encadeada /  especifíca o limite de temperatura
+        print("temperatura dentro do intervalo certo!") # mensagem na tela
+    else:
+        print("temperatura fora do padrão.") # mensagem se estiver temperatura fora do padrão
+    
+    print("leitura concluída.")
+    time.sleep(2)
 
-# se error(1)
-# fazer mais uma leitura do sistema
-# localizar o erro
-# informar no painel a falha
-
-# se ok 
-#iniciar contagem regressiva
-
-# se erro(2) abortar lançamento
+    resposta = input("deseja continuar?\n sim/não").lower()
+    if resposta == 'sim':
+        print("reiniciado o processo.")
+        continue # volta para o inicio de while
+    elif resposta == 'não':
+        print("passando para o próximo sensor.")
+        break # sai do looping
+    else:
+        print("resposta inválida, reiniciamento de segurança!!!")
+        break
